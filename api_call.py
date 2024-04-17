4e78dab8e04820fb1a31e6e6242275cbd1373723
Brock Houston <brockhouston@admins-MacBook-Pro.local>
2024-04-16 21:14:04 -0600

    now getting the correct output

--- api_call.py
+++ api_call.py
@@ -39,36 +39,39 @@ def get_top_show_story():
             print(data)
         """
 
-    data = []
-    response = requests.get('https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty')
+    data_show = []
+    response = requests.get('https://hacker-news.firebaseio.com/v0/showstories.json')
+    print(response.json())
     for top_story_id in response.json():  # top_story_id is each item in the list
         story_response = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{top_story_id}.json')
         story_response = story_response.json()
+        print(story_response)
 
         for key in story_response:
-            data.append((key, story_response[key]))
+            if( key == "by" or key == "descendants" or key == "title" or key == "id" or key == "url"):
+                data_show.append((key, story_response[key]))
         # Ensure the pops are within bounds
-            if len(data) > 8:  # Only pop if there are enough elements
-                data.pop(4)
-                data.pop(4)
-                data.pop(4)
-                data.pop(5)
-        print(data)
+           #if len(data) > 8:  # Only pop if there are enough elements
+           #    data.pop(4)
+           #    data.pop(4)
+           #    data.pop(4)
+           #    data.pop(5)
+    print(data_show)
     print("\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n")
 
 get_top_show_story()
 
 def get_job_posting():
-    data = []
+    data_jobs = []
     response = requests.get('https://hacker-news.firebaseio.com/v0/jobstories.json')
     for job_posting_id in response.json():
         job_response =  requests.get(f'https://hacker-news.firebaseio.com/v0/item/{job_posting_id}.json')
         job_response = job_response.json()
 
         for key in job_response:
-            data.append((key, job_response[key]))
-            #data.pop(2)
-        print(data)
+            if( key != "score" and  key != "type" ):
+                data_jobs.append((key, job_response[key]))
+    print(data_jobs)
 
 get_job_posting()
 
